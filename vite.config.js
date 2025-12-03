3db67732cfbc8e5f64fe0a87018a18865782fc29
Jared Barranco <barrancojared577@gmail.com>
2025-12-03 10:21:53 -0600

    Package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://jared-asahel.github.io/mi-trayectoria-jared-asahel",
+});
